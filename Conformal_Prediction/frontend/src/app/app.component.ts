f2397b63e7ec2b6003c63b3bffb112b710df818d
margaultmg <margault.mons-gouillaud@epfedu.fr>
2026-01-04 14:28:43 +0100

    amélioration

--- Conformal_Prediction/frontend/src/app/app.component.ts
+++ Conformal_Prediction/frontend/src/app/app.component.ts
@@ -126,39 +126,125 @@ export class AppComponent {
   featureGroups = [
     {
       name: 'Général',
-      features: ['MS SubClass', 'MS Zoning', 'Neighborhood', 'Bldg Type', 'House Style']
+      features: [
+        { original: 'MS SubClass', display: 'Sous-classe de la propriété' },
+        { original: 'MS Zoning', display: 'Classement du zonage' },
+        { original: 'Neighborhood', display: 'Quartier' },
+        { original: 'Bldg Type', display: 'Type de bâtiment' },
+        { original: 'House Style', display: 'Style de la maison' }
+      ]
     },
     {
       name: 'Lot',
-      features: ['Lot Frontage', 'Lot Area', 'Street', 'Alley', 'Lot Shape', 'Land Contour', 'Utilities', 'Lot Config', 'Land Slope']
+      features: [
+        { original: 'Lot Frontage', display: 'Façade du terrain' },
+        { original: 'Lot Area', display: 'Superficie du terrain' },
+        { original: 'Street', display: 'Type d\'accès à la rue' },
+        { original: 'Alley', display: 'Accès à l\'allée' },
+        { original: 'Lot Shape', display: 'Forme du terrain' },
+        { original: 'Land Contour', display: 'Topographie du terrain' },
+        { original: 'Utilities', display: 'Services publics' },
+        { original: 'Lot Config', display: 'Configuration du lot' },
+        { original: 'Land Slope', display: 'Pente du terrain' }
+      ]
     },
     {
       name: 'Qualité & Condition',
-      features: ['Overall Qual', 'Overall Cond', 'Exter Qual', 'Exter Cond', 'Bsmt Qual', 'Bsmt Cond', 'Heating QC', 'Kitchen Qual', 'Fireplace Qu', 'Garage Qual', 'Garage Cond', 'Pool QC', 'Fence']
+      features: [
+        { original: 'Overall Qual', display: 'Qualité générale du matériau et de la finition' },
+        { original: 'Overall Cond', display: 'État général' },
+        { original: 'Exter Qual', display: 'Qualité de l\'extérieur' },
+        { original: 'Exter Cond', display: 'État de l\'extérieur' },
+        { original: 'Bsmt Qual', display: 'Hauteur du sous-sol' },
+        { original: 'Bsmt Cond', display: 'État général du sous-sol' },
+        { original: 'Heating QC', display: 'Qualité et état du chauffage' },
+        { original: 'Kitchen Qual', display: 'Qualité de la cuisine' },
+        { original: 'Fireplace Qu', display: 'Qualité du foyer' },
+        { original: 'Garage Qual', display: 'Qualité du garage' },
+        { original: 'Garage Cond', display: 'État du garage' },
+        { original: 'Pool QC', display: 'Qualité de la piscine' },
+        { original: 'Fence', display: 'Qualité de la clôture' }
+      ]
     },
     {
       name: 'Construction',
-      features: ['Year Built', 'Year Remod/Add', 'Roof Style', 'Roof Matl', 'Exterior 1st', 'Exterior 2nd', 'Mas Vnr Type', 'Mas Vnr Area', 'Foundation', 'Heating', 'Central Air', 'Electrical', 'Functional']
+      features: [
+        { original: 'Year Built', display: 'Date de construction originale' },
+        { original: 'Year Remod/Add', display: 'Date de rénovation/agrandissement' },
+        { original: 'Roof Style', display: 'Type de toit' },
+        { original: 'Roof Matl', display: 'Matériau du toit' },
+        { original: 'Exterior 1st', display: 'Revêtement extérieur 1' },
+        { original: 'Exterior 2nd', display: 'Revêtement extérieur 2' },
+        { original: 'Mas Vnr Type', display: 'Type de placage de maçonnerie' },
+        { original: 'Mas Vnr Area', display: 'Superficie du placage de maçonnerie en pieds carrés' },
+        { original: 'Foundation', display: 'Type de fondation' },
+        { original: 'Heating', display: 'Type de chauffage' },
+        { original: 'Central Air', display: 'Climatisation centrale' },
+        { original: 'Electrical', display: 'Système électrique' },
+        { original: 'Functional', display: 'Évaluation fonctionnelle de la maison' }
+      ]
     },
     {
       name: 'Sous-sol',
-      features: ['Bsmt Exposure', 'BsmtFin Type 1', 'BsmtFin SF 1', 'BsmtFin Type 2', 'BsmtFin SF 2', 'Bsmt Unf SF', 'Total Bsmt SF', 'Bsmt Full Bath', 'Bsmt Half Bath']
+      features: [
+        { original: 'Bsmt Exposure', display: 'Exposition du sous-sol' },
+        { original: 'BsmtFin Type 1', display: 'Qualité de finition du sous-sol (Type 1)' },
+        { original: 'BsmtFin SF 1', display: 'Superficie finie du sous-sol (Type 1) en pieds carrés' },
+        { original: 'BsmtFin Type 2', display: 'Qualité de finition du sous-sol (Type 2)' },
+        { original: 'BsmtFin SF 2', display: 'Superficie finie du sous-sol (Type 2) en pieds carrés' },
+        { original: 'Bsmt Unf SF', display: 'Superficie non finie du sous-sol en pieds carrés' },
+        { original: 'Total Bsmt SF', display: 'Superficie totale du sous-sol en pieds carrés' },
+        { original: 'Bsmt Full Bath', display: 'Salles de bain complètes au sous-sol' },
+        { original: 'Bsmt Half Bath', display: 'Demi-salles de bain au sous-sol' }
+      ]
     },
     {
       name: 'Espaces de vie',
-      features: ['1st Flr SF', '2nd Flr SF', 'Low Qual Fin SF', 'Gr Liv Area', 'Full Bath', 'Half Bath', 'Bedroom AbvGr', 'Kitchen AbvGr', 'TotRms AbvGrd', 'Fireplaces']
+      features: [
+        { original: '1st Flr SF', display: 'Superficie du premier étage en pieds carrés' },
+        { original: '2nd Flr SF', display: 'Superficie du deuxième étage en pieds carrés' },
+        { original: 'Low Qual Fin SF', display: 'Superficie finie de faible qualité en pieds carrés' },
+        { original: 'Gr Liv Area', display: 'Superficie habitable au-dessus du sol en pieds carrés' },
+        { original: 'Full Bath', display: 'Salles de bain complètes au-dessus du sol' },
+        { original: 'Half Bath', display: 'Demi-salles de bain au-dessus du sol' },
+        { original: 'Bedroom AbvGr', display: 'Chambres au-dessus du sol' },
+        { original: 'Kitchen AbvGr', display: 'Cuisines au-dessus du sol' },
+        { original: 'TotRms AbvGrd', display: 'Pièces totales au-dessus du sol (sans salles de bain)' },
+        { original: 'Fireplaces', display: 'Nombre de foyers' }
+      ]
     },
     {
       name: 'Garage',
-      features: ['Garage Type', 'Garage Yr Blt', 'Garage Finish', 'Garage Cars', 'Garage Area', 'Paved Drive']
+      features: [
+        { original: 'Garage Type', display: 'Localisation du garage' },
+        { original: 'Garage Yr Blt', display: 'Année de construction du garage' },
+        { original: 'Garage Finish', display: 'Finition intérieure du garage' },
+        { original: 'Garage Cars', display: 'Taille du garage en capacité de voiture' },
+        { original: 'Garage Area', display: 'Superficie du garage en pieds carrés' },
+        { original: 'Paved Drive', display: 'Allée pavée' }
+      ]
     },
     {
       name: 'Extérieur',
-      features: ['Wood Deck SF', 'Open Porch SF', 'Enclosed Porch', '3Ssn Porch', 'Screen Porch', 'Pool Area', 'Misc Feature', 'Misc Val']
+      features: [
+        { original: 'Wood Deck SF', display: 'Superficie du pont en bois en pieds carrés' },
+        { original: 'Open Porch SF', display: 'Superficie du porche ouvert en pieds carrés' },
+        { original: 'Enclosed Porch', display: 'Superficie du porche fermé en pieds carrés' },
+        { original: '3Ssn Porch', display: 'Superficie du porche trois saisons en pieds carrés' },
+        { original: 'Screen Porch', display: 'Superficie du porche grillagé en pieds carrés' },
+        { original: 'Pool Area', display: 'Superficie de la piscine en pieds carrés' },
+        { original: 'Misc Feature', display: 'Caractéristique diverse non couverte par d\'autres catégories' },
+        { original: 'Misc Val', display: 'Valeur de la caractéristique diverse' }
+      ]
     },
     {
       name: 'Vente',
-      features: ['Mo Sold', 'Yr Sold', 'Sale Type', 'Sale Condition']
+      features: [
+        { original: 'Mo Sold', display: 'Mois de vente' },
+        { original: 'Yr Sold', display: 'Année de vente' },
+        { original: 'Sale Type', display: 'Type de vente' },
+        { original: 'Sale Condition', display: 'Condition de vente' }
+      ]
     }
   ];
 
--- Conformal_Prediction/frontend/src/app/app.config.ts
+++ Conformal_Prediction/frontend/src/app/app.config.ts
@@ -1,10 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import fr from '@angular/common/locales/fr';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
+registerLocaleData(fr);
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +17,6 @@ export const appConfig: ApplicationConfig = {
     provideClientHydration(),
     provideAnimationsAsync(),
     provideHttpClient(withFetch()),
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
   ]
 };
